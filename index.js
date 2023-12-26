e7a6282399a6afec8f9e4c08ca8e08074a5fdf22
Victor Antonio Martinez Solis <victormartinezsolis20@gmail.com>
2023-12-26 13:58:25 -0600

    🎉 Creación del proyecto

---
+++ index.js
@@ -0,0 +1,46 @@
+//* va nuestro código (servidor)
+const express = require('express');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const port = 3000;
+
+const Animal = mongoose.model('Animal', new mongoose.Schema({
+    tipo: String,
+    estado: String,
+}))
+
+const app = express();
+
+//Conexión a mongo
+mongoose.connect('mongodb://devf:password@monguitodos:27017/miapp?authSource=admin');
+
+// Mensaje de bienvenida
+app.get('/', async(req,res)=>{
+    res.setHeader('Content-Type', 'text/html')
+    res.status(200)
+    res.send('<h1>ESTÁ FUNCIONANDO</h1>')
+})
+
+//Buscar un animalito (listar)
+app.get('/listar', async(req,res)=>{
+    console.log('listando... cochinitos...');
+    const animales = await Animal.find()
+    return res.send(animales)
+})
+
+
+//Crear un animalito
+app.get('/crear', async(req,res)=>{
+    console.log('creando...');
+    await Animal.create({tipo: 'Cochinito', estado: 'Feliz'})
+    await Animal.create({tipo: 'Gatito', estado: 'Programando'})
+    await Animal.create({tipo: 'Perrito', estado: 'Comiendo'})
+    await Animal.create({tipo: 'Conejito', estado: 'Saltando'})
+    res.status(200)
+    return res.send('Created...')
+})
+
+//Habilitar el puerto/servidor
+app.listen(port, ()=>{
+    console.log("Servidor corriendo en el puerto:".bgBlue + " " + `http://localhost:${port}`.underline);
+})
